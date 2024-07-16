217e6d17851283bc8d155c3fc5279b09f447a749
hanni66 <haeunkim0807@gmail.com>
2024-07-16 16:08:22 +0900

    chore : ReviewResponse의 from builder 적용

--- src/main/java/jjhhyb/deepvalley/community/dto/response/ReviewResponse.java
+++ src/main/java/jjhhyb/deepvalley/community/dto/response/ReviewResponse.java
@@ -31,27 +31,23 @@ public class ReviewResponse {
 
     // Review 객체를 ReviewResponse 객체로 변환
     public static ReviewResponse from(Review review) {
-        ReviewResponse response = new ReviewResponse();
-        response.setReviewId(review.getReviewId());
-        response.setTitle(review.getTitle());
-        response.setRating(review.getRating().name());
-        response.setContent(review.getContent());
-        response.setVisitedDate(review.getVisitedDate() != null ? review.getVisitedDate() : null);
-        response.setPrivacy(review.getPrivacy().name());
-        response.setMemberId(review.getMemberId());
-        response.setPlaceId(review.getPlaceId());
-        response.setCreatedDate(review.getCreatedDate());
-        response.setUpdatedDate(review.getUpdatedDate());
-
-        // 이미지 URL 리스트 생성
-        response.setImageUrls(review.getReviewImages().stream()
-                .map(reviewImage -> reviewImage.getImage().getImageUrl())
-                .collect(Collectors.toList()));
-
-        // 태그 이름 리스트 생성
-        response.setTagNames(review.getReviewTags().stream()
-                .map(reviewTag -> reviewTag.getTag().getName())
-                .collect(Collectors.toList()));
-        return response;
+        return ReviewResponse.builder()
+                .reviewId(review.getReviewId())
+                .title(review.getTitle())
+                .rating(review.getRating().name())
+                .content(review.getContent())
+                .visitedDate(review.getVisitedDate())
+                .privacy(review.getPrivacy().name())
+                .memberId(review.getMemberId())
+                .placeId(review.getPlaceId())
+                .createdDate(review.getCreatedDate())
+                .updatedDate(review.getUpdatedDate())
+                .imageUrls(review.getReviewImages().stream()
+                        .map(reviewImage -> reviewImage.getImage().getImageUrl())
+                        .collect(Collectors.toList()))
+                .tagNames(review.getReviewTags().stream()
+                        .map(reviewTag -> reviewTag.getTag().getName())
+                        .collect(Collectors.toList()))
+                .build();
     }
 }
