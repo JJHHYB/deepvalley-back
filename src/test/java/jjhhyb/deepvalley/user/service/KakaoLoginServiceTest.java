cd1954c9ddb0747d4e8d595f7322667646edc456
Berygna <berygna@gmail.com>
2024-07-19 13:28:58 +0900

    chore: config https

--- src/test/java/jjhhyb/deepvalley/user/service/KakaoLoginServiceTest.java
+++ src/test/java/jjhhyb/deepvalley/user/service/KakaoLoginServiceTest.java
@@ -9,14 +9,24 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "oauth.kakao.client-id=0254b79556922c41c5f92bfb2ddc63fa",
+        "oauth.kakao.redirect-uri=http://localhost:3000/auth",
+        "oauth.kakao.url.auth=https://kauth.kakao.com",
+        "oauth.kakao.url.api=https://kapi.kakao.com"
+})
 class KakaoLoginServiceTest {
 
     @InjectMocks
@@ -36,14 +46,15 @@ class KakaoLoginServiceTest {
         // Given
         String authorizationCode = "test_code";
         KakaoTokenDto kakaoTokenDto = new KakaoTokenDto();
-        when(restTemplate.postForEntity(anyString(), any(), eq(KakaoTokenDto.class))).thenReturn(new ResponseEntity<>(kakaoTokenDto, HttpStatus.OK));
+        when(restTemplate.postForEntity(eq("https://kauth.kakao.com/oauth/token"), any(HttpEntity.class), eq(KakaoTokenDto.class)))
+                .thenReturn(new ResponseEntity<>(kakaoTokenDto, HttpStatus.OK));
 
         // When
         KakaoTokenDto result = kakaoLoginService.getAccessToken(authorizationCode);
 
         // Then
         assertNotNull(result);
-        verify(restTemplate, times(1)).postForEntity(anyString(), any(), eq(KakaoTokenDto.class));
+        verify(restTemplate, times(1)).postForEntity(eq("https://kauth.kakao.com/oauth/token"), any(HttpEntity.class), eq(KakaoTokenDto.class));
     }
 
     @Test
