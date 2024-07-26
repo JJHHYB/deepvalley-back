ef84432d426cebbb4ecf12383778e3825b410c0d
hanni66 <haeunkim0807@gmail.com>
2024-07-26 18:15:35 +0900

    feat : JsonNaming 어노테이션 추가

--- src/main/java/jjhhyb/deepvalley/community/dto/response/PlaceImageResponse.java
+++ src/main/java/jjhhyb/deepvalley/community/dto/response/PlaceImageResponse.java
@@ -1,5 +1,7 @@
 package jjhhyb.deepvalley.community.dto.response;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
 
 import java.util.List;
@@ -8,6 +10,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class PlaceImageResponse {
     private Long reviewId;
     private List<String> imageUrls;
