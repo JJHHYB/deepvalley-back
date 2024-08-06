3464b0d3deaa9f889d10c17fe0ec191c9c54cab3
hanni66 <haeunkim0807@gmail.com>
2024-08-06 16:09:55 +0900

    feat : 배너 이미지 s3 에서 가져오기 테스트 코드 수정

--- src/test/java/jjhhyb/deepvalley/banner/BannerServiceTest.java
+++ src/test/java/jjhhyb/deepvalley/banner/BannerServiceTest.java
@@ -1,19 +1,38 @@
 package jjhhyb.deepvalley.banner;
 
 import jjhhyb.deepvalley.banner.dto.response.BannerResponse;
+import jjhhyb.deepvalley.image.S3Service;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 @SpringBootTest
 public class BannerServiceTest {
+    @Autowired
+    private BannerService bannerService;
+
+    @MockBean
+    private S3Service s3Service;
     @Test
     @DisplayName("[GET] 메인 배너 조회")
     void getBanner() {
         // Arrange
-        BannerService bannerService = new BannerServiceImpl();
+        List<String> mockImageUrls = List.of(
+                "https://deep-valley-image.s3.ap-northeast-2.amazonaws.com/banner-images/Group_37.png",
+                "https://deep-valley-image.s3.ap-northeast-2.amazonaws.com/banner-images/Group_41.png",
+                "https://deep-valley-image.s3.ap-northeast-2.amazonaws.com/banner-images/Group_43.png",
+                "https://deep-valley-image.s3.ap-northeast-2.amazonaws.com/banner-images/Group_44.png",
+                "https://deep-valley-image.s3.ap-northeast-2.amazonaws.com/banner-images/Group_46.png"
+        );
+
+        given(s3Service.listFilesInFolder("banner-images")).willReturn(mockImageUrls);
 
         // Act
         BannerResponse bannerResponse = bannerService.getBanner();
@@ -21,12 +40,6 @@ public class BannerServiceTest {
         // Assert
         assertThat(bannerResponse).isNotNull();
         assertThat(bannerResponse.getImageUrls()).isNotEmpty();
-        assertThat(bannerResponse.getImageUrls()).containsExactly(
-                "https://example.com/banner1.jpg",
-                "https://example.com/banner2.jpg",
-                "https://example.com/banner3.jpg",
-                "https://example.com/banner4.jpg",
-                "https://example.com/banner5.jpg"
-        );
+        assertThat(bannerResponse.getImageUrls()).containsExactlyElementsOf(mockImageUrls);
     }
 }
