5774738fdffd3ef65378d8722c3aebc07d65cc78
sshinylee <sunshinylee0@gmail.com>
2024-08-09 17:06:05 +0900

    feat: Add sorting to valley search (#123)

--- src/main/java/jjhhyb/deepvalley/place/valley/repository/CustomizedValleyRepository.java
+++ src/main/java/jjhhyb/deepvalley/place/valley/repository/CustomizedValleyRepository.java
@@ -1,10 +1,11 @@
 package jjhhyb.deepvalley.place.valley.repository;
 
+import jjhhyb.deepvalley.place.PlaceSortType;
 import jjhhyb.deepvalley.place.valley.dto.ValleyResponse;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CustomizedValleyRepository {
-    List<ValleyResponse> searchValleys(Optional<String> keyword, Optional<String> region, Optional<List<Double>> position, Optional<List<String>> tagNames, Long radius, Optional<Double> rating, Long offset);
+    List<ValleyResponse> searchValleys(Optional<String> keyword, Optional<String> region, Optional<List<Double>> position, Optional<List<String>> tagNames, Long radius, Optional<Double> rating, Optional<PlaceSortType> sortType, Long offset);
 }
